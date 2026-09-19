b2a3ed3da84c3398b555e465f268f6f042cc5d1a
muazdev <funzoft.muaz@gmail.com>
2026-09-19 19:01:29 +0500

    Compile library modules against android-37 for Compose Multiplatform 1.12.0

--- build-logic/src/main/kotlin/hijri.multiplatform.library.gradle.kts
+++ build-logic/src/main/kotlin/hijri.multiplatform.library.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
     android {
         namespace = project.findProperty("android.namespace") as? String
             ?: "com.muazdev.hijricalendar.${project.name.replace("-", ".")}"
-        compileSdk = 36
+        compileSdk = 37
         minSdk = 26
 
         compilerOptions {
